7ea6ce2bc211d545d38699ce95a8846577cd24ee
Artem Stipnek <stipnekwow@gmail.com>
2023-02-23 09:29:47 -0500

    chat folder

--- src/components/Chats/Chat/Chat.jsx
+++ src/components/Chats/Chat/Chat.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Add  from "../imgs/add.png";
 import Cam  from "../imgs/cam.png";
 import More  from "../imgs/more.png";
-import Messages from './Messages';
-import Input from './Input';
+import Messages from '../../Messages';
+import Input from '../../Input';
 
 export const Chat = () => {
   return (
