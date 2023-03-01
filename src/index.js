d3a536396ee18211fc7bac32b5bc199b8c57f3a8
damir-mu <113216547+Damir-Mu@users.noreply.github.com>
2023-03-02 01:42:40 +0600

    update1.2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthContextProvider } from './context/AuthContext';
+import { AuthContextProvider } from './components/context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
