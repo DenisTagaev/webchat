8789fac6484ab37e3935fbcf44656fab8745c268
Denis Tagaev <sivean.27@gmail.com>
2023-03-16 13:32:09 -0400

    Fixed register icon

--- src/pages/registration/Register.jsx
+++ src/pages/registration/Register.jsx
@@ -4,7 +4,7 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { auth, storage, db } from "../../environments/firebase";
-import AddImg from "../../imgs/addAvatar.png";
+import { BiImageAdd } from "react-icons/bi";
 import Logbar from "../../components/Logbar/Logbar";
 import "./Register.scss";
 
@@ -203,15 +203,14 @@ const Register = () => {
             <span className="formError">{errors.repeatPassword}</span>
           )}
           <label id="avatarInput">
-            <img id="avatarImage" src={AddImg} alt="Chose avatar placeholder" />
+            {/* <img id="avatarImage" src={AddImg} alt="Chose avatar placeholder" /> */}
+            <BiImageAdd className="regImg" />
             <span>Chose avatar</span>
             {/* to customize standard input look we hide the input element and wrap it in a
                         label with desired output content */}
             <input type="file" hidden={true} />
           </label>
-          {imgError && (
-            <span className="formError">{imgError}</span>
-          )}
+          {imgError && <span className="formError">{imgError}</span>}
           <button id="registerSubmit" type="submit">
             Sign up
           </button>
