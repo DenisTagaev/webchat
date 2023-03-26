404c27ba2c7b665d74c4a7933f79d08909bd9ac9
Denis Tagaev <sivean.27@gmail.com>
2023-03-25 23:10:38 -0400

    Added feature to show the time message was sent

--- src/components/Messages/Message/Message.jsx
+++ src/components/Messages/Message/Message.jsx
@@ -1,23 +1,39 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { ChatContext } from "../../context/ChatContext";
-import './Message.scss';
+import "./Message.scss";
 
-export const Message = ({message}) => {
+export const Message = ({ message }) => {
   //getting the data about the user from the firebase
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
   const ref = useRef();
 
+  //function gets the numeric value of time message was sent and converts
+  //it back to the human readable format
+  const getMessageTime = (message) => {
+    // Create a new Date object using the server timestamp
+    const date = new Date(message.date.seconds * 1000);
+    // Get the local time and date components
+    const localTime = date.toLocaleTimeString([], {
+      hour12: true,
+      hourCycle: "h12",
+      hour: "numeric",
+      minute: "numeric",
+    });
+    return localTime;
+  };
+
   useEffect(() => {
-    ref.current?.scrollIntoView({behavior: 'smooth'})
+    ref.current?.scrollIntoView({ behavior: "smooth" });
   }, [message]);
 
   return (
     <div
       ref={ref}
-      className={`messageContainer ${message.senderId === currentUser.uid && "owner"}`}
-      // className="message owner"
+      className={`messageContainer ${
+        message.senderId === currentUser.uid && "owner"
+      }`}
     >
       <div className="messageInfo">
         <img
@@ -28,7 +44,7 @@ export const Message = ({message}) => {
           }
           alt="profile avatar"
         />
-        <span>Just now</span>
+        <span>{getMessageTime(message)}</span>
       </div>
       <div className="messageContent">
         <p>{message.text}</p>
@@ -36,6 +52,6 @@ export const Message = ({message}) => {
       </div>
     </div>
   );
-}
+};
 
 export default Message;
